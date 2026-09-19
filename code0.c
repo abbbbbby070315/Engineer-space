022fd499666c0dd981ef68fe23e2ea3a0f25751c
abbbbbby070315 <abigailcajalelizalde@gmail.com>
2026-09-19 20:56:26 -0300

    Update and rename code0.c to code0.java

--- code0.c
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-#include <stdio.h>
-
-std namespace;
-cout << "hello world!" <<;
---
+++ code0.java
@@ -0,0 +1,5 @@
+public class code0{
+  public static void main(String []args){
+    System.out.println("Hello world!");
+  }
+}
